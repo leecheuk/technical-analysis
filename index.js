a542e7c12abffa1d8fbbcb540a54e74faf047bc6
m <dev.cheuk.lee@gmail.com>
2019-09-21 17:18:08 -0400

    added sum function

--- index.js
+++ index.js
@@ -9,4 +9,16 @@ function sum(arr) {
     return arr.reduce((accumulator, currentValue) => accumulator + currentValue);
 }
 
-module.exports = {sum};
+/**
+ * Returns variance of a list of numbers
+ * 
+ * @param {Array.<number>} arr A list of numbers
+ * 
+ * @return {number} Variance of the list of numbers
+ */
+function variance(arr) {
+    var mean = sum(arr)/arr.length;
+    return sum(arr.map(n => Math.pow(n-mean, 2)))/(arr.length-1);
+}
+
+module.exports = {sum, variance};
