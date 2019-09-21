18f3e59a1bd2373aa4a28a3ac639d8d04bec9324
m <dev.cheuk.lee@gmail.com>
2019-09-21 17:18:56 -0400

    added simpleMovingAverage function

--- index.js
+++ index.js
@@ -21,4 +21,28 @@ function variance(arr) {
     return sum(arr.map(n => Math.pow(n-mean, 2)))/(arr.length-1);
 }
 
-module.exports = {sum, variance};
+/**
+ * Simple moving average method.
+ * 
+ * @param {Object.<string, number>} data Collection of closing price of trading days 
+ * @param {number} days Number of time periods in days
+ * 
+ * @return {Object.<string, number>} Collection of average closing price for trading days
+ */
+function simpleMovingAverage(data, days) {
+    var temp = [];
+    var sma = {}
+    var prices = Object.entries(data)
+    for (var [i, [date, price]] of prices.entries()) {
+        temp.push(price);
+        if (i+1 == days) {
+            sma[date] = sum(temp)/temp.length;
+        } else if (i+1 > days) {
+            temp.shift();
+            sma[date] = sum(temp)/temp.length;
+        }
+    }
+    return sma;
+}
+
+module.exports = {sum, variance, simpleMovingAverage};
